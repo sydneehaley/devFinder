cc1f352495ac2f2466fbe10cb51261981b46074e
Sydnee Haley <45055658+sydneehaley@users.noreply.github.com>
2022-10-24 18:17:58 -0400

    conversion to typescript

---
+++ client/pages/api/jobs/jobs.tsx
@@ -0,0 +1,7 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../../../lib/prisma/client/client";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const jobs = await prisma.jobs.findMany();
+  res.status(200).json(jobs);
+};
---
+++ lib/prisma/client/client.ts
@@ -0,0 +1,30 @@
+// const { PrismaClient } = require("@prisma/client");
+
+// let prisma;
+
+// if (process.env.NODE_ENV === "production") {
+//   prisma = new PrismaClient();
+// } else {
+//   if (!global.prisma) {
+//     global.prisma = new PrismaClient();
+//   }
+//   prisma = global.prisma;
+// }
+
+// module.exports = { prisma };
+
+import { PrismaClient } from "@prisma/client";
+
+// add prisma to the NodeJS global type
+interface CustomNodeJsGlobal extends NodeJS.Global {
+  prisma: PrismaClient;
+}
+
+// Prevent multiple instances of Prisma Client in development
+declare const global: CustomNodeJsGlobal;
+
+const prisma = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV === "development") global.prisma = prisma;
+
+export default prisma;
