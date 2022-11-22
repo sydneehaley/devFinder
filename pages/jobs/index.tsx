f36d4a50c6e190c64914713d438b426504b4be31
sydneehaley <hello@sydneehaley.com>
2022-11-22 16:49:03 -0500

    swr test

--- pages/jobs/index.tsx
+++ pages/jobs/index.tsx
@@ -5,12 +5,17 @@ import useSWR, { SWRConfig } from 'swr';
 import Filters from '../../components/jobs/Filters';
 import List from '../../components/jobs/List';
 import { InferGetServerSidePropsType } from 'next';
-import type { Fetcher } from 'swr';
+import type { Fetcher, Key } from 'swr';
 
 const dev = process.env.NODE_ENV !== 'production';
 const server = dev ? 'http://localhost:3000' : 'https://dev-finder-sydneehaley.vercel.app/';
-const fetcher: Fetcher<User, string> = (url) => fetch(url).then((res) => res.json());
-const API = `${server}/api/jobs`;
+let API = `${server}/api/jobs`;
+
+if (global.window) {
+  API = '/api/jobs';
+}
+
+const fetcher: Fetcher<User, string> = (...args) => fetch(...args).then((res) => res.json());
 
 type User = any;
 
@@ -26,9 +31,11 @@ export async function getServerSideProps() {
 }
 
 const JobsList = () => {
-  const { data } = useSWR<any[]>(API);
+  const { data } = useSWR<any[]>(API, fetcher);
 
   console.log('Is data ready?', !!data);
+  console.log(data);
+  console.log(API);
 
   return (
     <div className='w-[82%] flex flex-col items-start justify-start'>
@@ -37,7 +44,7 @@ const JobsList = () => {
   );
 };
 
-function Jobs({ fallback }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+function Jobs() {
   return (
     <Layout>
       <Head>
@@ -47,12 +54,30 @@ function Jobs({ fallback }: InferGetServerSidePropsType<typeof getServerSideProp
 
       <div className='w-[82%] flex my-[5rem]'>
         <Filters />
-        <SWRConfig value={{ fallback }}>
-          <JobsList />
-        </SWRConfig>
+        {/* <SWRConfig value={{ fallback }}> */}
+        <JobsList />
+        {/* </SWRConfig> */}
       </div>
     </Layout>
   );
 }
+
+// function Jobs({ fallback }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+//   return (
+//     <Layout>
+//       <Head>
+//         <title>devFinder</title>
+//         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+//       </Head>
+
+//       <div className='w-[82%] flex my-[5rem]'>
+//         <Filters />
+//         <SWRConfig value={{ fallback }}>
+//           <JobsList />
+//         </SWRConfig>
+//       </div>
+//     </Layout>
+//   );
+// }
 
 export default Jobs;
