bc7c2002be978b6c97ddcab665e690a2c17f5fb4
sydneehaley <hello@sydneehaley.com>
2023-01-03 01:41:00 -0500

    swr update

--- pages/jobs/index.tsx
+++ pages/jobs/index.tsx
@@ -8,17 +8,15 @@ import { InferGetServerSidePropsType } from 'next';
 import type { Fetcher, Key } from 'swr';
 
 const dev = process.env.NODE_ENV !== 'production';
-const server = dev ? 'http://localhost:3000' : 'https://dev-finder-sydneehaley.vercel.app';
-let API = `${server}/api/jobs`;
+// const API = dev ? '/api/jobs' : 'https://dev-finder-sydneehaley.vercel.app';
+const API = '/api/jobs';
 
-// if (global.window) {
-//   API = '/api/jobs';
-// }
+console.log(API);
 
 type Jobs = any;
-const fetcher: Fetcher<Jobs, string> = (...args) => fetch(...args).then((res) => res.json());
+const fetcher: Fetcher<Jobs, Key> = (url: any) => fetch(url).then((res) => res.json());
 
-export async function getServerSideProps() {
+async function getServerSideProps() {
   const jobs = await fetcher(API);
   return {
     props: {
@@ -29,37 +27,9 @@ export async function getServerSideProps() {
   };
 }
 
-// export async function getServerSideProps() {
-//   const res = await fetch(API);
-//   const data = await res.json();
-//   return {
-//     props: {
-//       data,
-//     },
-//   };
-// }
-
-interface Props {
-  data: any;
-}
-
-// const JobsList: React.FC<Props> = ({ data }) => {
-//   // const { data } = useSWR<any[]>('/api/jobs', fetcher);
-//   // console.log('Is data ready?', !!data);
-//   console.log(data);
-
-//   return (
-//     <div className='w-[82%] flex flex-col items-start justify-start'>
-//       <List data={data} />
-//     </div>
-//   );
-// };
-
 function JobsList() {
-  const { data } = useSWR<any[]>(API, fetcher);
+  const { data, error } = useSWR<any[]>(API, fetcher);
   console.log('Is data ready?', !!data);
-  console.log(data);
-  console.log(API);
 
   return (
     <div className='w-[82%] flex flex-col items-start justify-start'>
@@ -67,22 +37,6 @@ function JobsList() {
     </div>
   );
 }
-
-// function Jobs({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-//   return (
-//     <Layout>
-//       <Head>
-//         <title>devFinder</title>
-//         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-//       </Head>
-
-//       <div className='w-[82%] flex my-[5rem]'>
-//         <Filters />
-//         <JobsList data={data} />
-//       </div>
-//     </Layout>
-//   );
-// }
 
 function Jobs({ fallback }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
@@ -94,9 +48,9 @@ function Jobs({ fallback }: InferGetServerSidePropsType<typeof getServerSideProp
 
       <div className='w-[82%] flex my-[5rem]'>
         <Filters />
-        <SWRConfig value={{ fallback }}>
-          <JobsList />
-        </SWRConfig>
+        {/* <SWRConfig value={{ fallback }}> */}
+        <JobsList />
+        {/* </SWRConfig> */}
       </div>
     </Layout>
   );
