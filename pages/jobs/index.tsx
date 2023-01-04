25eb72cea9e68b37c70b44097ff05032fc546b2e
sydneehaley <hello@sydneehaley.com>
2023-01-03 21:45:12 -0500

    swr fixes

--- pages/jobs/index.tsx
+++ pages/jobs/index.tsx
@@ -6,18 +6,32 @@ import Filters from '../../components/jobs/Filters';
 import List from '../../components/jobs/List';
 import { InferGetServerSidePropsType } from 'next';
 import type { Fetcher, Key } from 'swr';
+import { unstable_serialize } from 'swr/infinite';
+import axios from 'axios';
 
 const dev = process.env.NODE_ENV !== 'production';
-// const API = dev ? '/api/jobs' : 'https://dev-finder-sydneehaley.vercel.app';
-const API = '/api/jobs';
-
+const API = `http://localhost:3000/api/jobs`;
 console.log(API);
 
 type Jobs = any;
-const fetcher: Fetcher<Jobs, Key> = (url: any) => fetch(url).then((res) => res.json());
+const url: Key = '/api/jobs';
+// const fetcher: Fetcher<Jobs, string> = (...args) => fetch(...args).then((res) => res.json());
+
+const getJobs = async (url: string) => {
+  try {
+    const response = await axios.get(url);
+    console.log(response);
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const fetcher: Fetcher<Jobs, string> = (url) => getJobs(url);
 
 async function getServerSideProps() {
   const jobs = await fetcher(API);
+
   return {
     props: {
       fallback: {
@@ -28,12 +42,16 @@ async function getServerSideProps() {
 }
 
 function JobsList() {
-  const { data, error } = useSWR<any[]>(API, fetcher);
+  const { data, error } = useSWR('api/jobs', fetcher);
   console.log('Is data ready?', !!data);
+  console.log(data?.data);
+
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
 
   return (
     <div className='w-[82%] flex flex-col items-start justify-start'>
-      <List data={data} />
+      <List data={data?.data} />
     </div>
   );
 }
