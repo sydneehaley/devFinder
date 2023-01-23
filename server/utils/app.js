d82fd4097744f7259d65dc4803ecf6d340f5cee1
sydneehaley <hello@sydneehaley.com>
2023-01-22 20:07:55 -0500

    refactoring

--- server/utils/app.js
+++
@@ -1,8 +0,0 @@
-const createApp = (app, api, express) => {
-  app.use(express.json()); //parsing middleware
-  app.use(express.urlencoded({ extended: true })); //parsing middleware
-  app.use('/', api);
-  return app;
-};
-
-module.exports = { createApp };
