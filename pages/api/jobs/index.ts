a337e572ca17ae805da13e56658743cebf559456
sydneehaley <hello@sydneehaley.com>
2023-01-25 02:20:52 -0500

    daisyui && supabase auth added

--- pages/api/jobs/index.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import prisma from '../../../lib/prisma/client/client';
-
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const jobs = await prisma.jobs.findMany();
-  res.status(200).json(jobs);
-};
-
-export default handler;
