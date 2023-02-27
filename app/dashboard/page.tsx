1615967b59d219ddb55f13c83ce9f0f7e3ea48a7
sydneehaley <hello@sydneehaley.com>
2023-02-27 00:04:13 -0500

    strapi api auth update

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,19 +1,14 @@
 import 'server-only';
-import { createServerClient } from '../../utils/supabase-server';
 import { getJobs } from '../data/jobs';
 import List from '../../components/List';
-import Jobs from './jobs';
 
 export const revalidate = 0;
 
 export default async function Dashboard() {
-  // const supabase = createServerClient();
-  // const { data } = await supabase.from('test').select('*');
   const data = await getJobs('https://devfinder-client-api.herokuapp.com/api/jobs?populate=*');
 
   return (
     <div className='w-[80%] flex flex-col items-center justify-center my-[3rem] antialiased'>
-      {/* <Jobs serverPosts={data || []} /> */}
       <List data={data} />
     </div>
   );
