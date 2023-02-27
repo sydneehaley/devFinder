2b12a974e122ca0b0afdc23b8a00932be72f1d4d
sydneehaley <hello@sydneehaley.com>
2023-02-27 00:54:20 -0500

    strapi api auth feature added

--- hooks/auth/useCurrentUser.ts
+++ hooks/auth/useCurrentUser.ts
@@ -1,13 +1,27 @@
 import { useEffect, useState } from 'react';
 import Cookies from 'js-cookie';
 import { User } from '../../types/User';
+import jwt from 'jsonwebtoken';
 
 export const useCurrentUser = () => {
   const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
+    function isTokenExpired(token: any): boolean {
+      try {
+        const decodedToken = jwt.decode(token) as { exp: number };
+        const currentTime = Date.now() / 1000;
+        return decodedToken.exp < currentTime;
+      } catch (err) {
+        // Invalid token
+        return true;
+      }
+    }
+
+    const expired = isTokenExpired(Cookies.get('jwt'));
+
     const currentUser = Cookies.get('currentUser');
-    if (currentUser) {
+    if (currentUser && !expired) {
       setUser(JSON.parse(currentUser));
     }
   }, []);
