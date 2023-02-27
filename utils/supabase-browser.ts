dccf780cedadb1ffcd0a063b40eb32f982cbb4cc
sydneehaley <hello@sydneehaley.com>
2023-02-26 23:39:52 -0500

    strapi auth feature added

--- utils/supabase-browser.ts
+++
@@ -1,4 +0,0 @@
-import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
-import { Database } from '../types/database.types';
-
-export const createBrowserClient = () => createBrowserSupabaseClient<Database>();
--- utils/supabase-server.ts
+++
@@ -1,9 +0,0 @@
-import { headers, cookies } from 'next/headers';
-import { createServerComponentSupabaseClient } from '@supabase/auth-helpers-nextjs';
-import { Database } from '../types/database.types';
-
-export const createServerClient = () =>
-  createServerComponentSupabaseClient<Database>({
-    headers,
-    cookies,
-  });
