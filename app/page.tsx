c6da21d10e1b4fa33c8064d7b7aa9f60036fde4e
sydneehaley <hello@sydneehaley.com>
2023-03-26 21:19:11 -0400

    general ui updates

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,7 @@ import { data } from './data/test_jobs';
 export default async function Index() {
   // const data = await getJobs(`${process.env.NEXT_PUBLIC_STRAPI_API_URL_JOBS}
   return (
-<<<<<<< HEAD
     <div className='w-full text-neutral flex flex-col items-center justify-center'>
-=======
-    <div className='w-full  flex flex-col items-center justify-center text-neutral-600 dark:text-neutral-500 antialiased font-theme'>
->>>>>>> 36ade5e (general ui updates)
       <Navbar />
       <div className='w-full'>
         <List data={data} />
--- components/List.tsx
+++ components/List.tsx
@@ -41,7 +41,6 @@ export default function List(data: any) {
   console.log(data?.data?.data);
 
   return (
-<<<<<<< HEAD
     <div className='w-full flex flex-col'>
       <Searchbar />
       <div className='w-full flex relative z-0'>
@@ -114,50 +113,12 @@ export default function List(data: any) {
 
                       <div className='border-t border-b flex py-[1rem] mb-[3rem]'>
                         <div className='w-[14.5rem] flex items-center justify-between'>
-                          <button className='border border-neutral/40  rounded-full  py-[8px] px-[16px]  h-[4vh] font-medium text-[16px] flex items-center justify-start'>Apply</button>
-                          <button className='border border-neutral/40  rounded-full  py-[8px] px-[16px]  h-[4vh] font-medium text-[16px] flex items-center justify-start'>Report</button>
-=======
-    <div className='w-full  flex flex-col items-center justify-center'>
-      <Searchbar />
-      <div className='w-full  overflow-auto flex flex-col items-center justify-center'>
-        <div className='w-[95%] flex items-center'>
-          <div className='w-full'>
-            {/* <div className='flex flex-col pl-[2rem] mt-[3rem] mb-[3rem] leading-[3rem]'>
-              <h6 className='  text-[28px] leading-[3rem] font-regular text-neutral-800 dark:text-base-100'>Jobs for you</h6>
-              <p className='text-[16px]'>Showing 6 jobs</p>
-            </div> */}
-            <div className='w-full   h-full rounded-xl   mb-[2rem]'>
-              <ul className=' grid grid-cols-4  list-none h-full'>
-                {data?.data?.data?.map((job: any, i: any) => (
-                  <li
-                    key={i}
-                    className='col-span-4  bg-transparent dark:border-neutral-400/20  border-b flex items-center  justify-start h-full cursor-pointer p-[2rem]'
-                  >
-                    <div className='w-full leading-[1.75rem] w-[92%] flex flex-col'>
-                      {/* <button className='h-[4rem] w-[4rem] rounded-full flex items-center justify-center  mr-[1.5rem] mt-[0.5rem]'>
-                        <BuildingOffice2Icon className='h-9 w-9' />
-                      </button> */}
-                      <div className='mt-[1rem]'>
-                        <div>
-                          <h6 className='text-[20px] text-neutral-700 dark:text-neutral-200 font-semibold leading-[3rem]'>
-                            {job?.attributes.job_title}
-                          </h6>
-                          <h6 className='text-[14px] font-regular'>{job?.attributes.company_id.data?.attributes?.company_name}</h6>
-                          <h6 className='text-[14px] font-regular'>{job?.attributes.location}</h6>
-                        </div>
-                        {/* <p className='text-[14px] mt-[2rem] h-[5vh] overflow-hidden w-[60%] '>{job?.attributes.description}</p> */}
-                        <div className='mt-[3rem] flex'>
-                          {job?.attributes.remote ? (
-                            <button className='border dark:border-neutral-400/20 rounded-full px-[10px] py-[3px] max-w-[200px] font-semibold text-[13px] flex items-center justify-center mr-[8px]'>
-                              <GlobeAltIcon className='w-6 h-6  mr-[0.5rem]' /> Remote
-                            </button>
-                          ) : null}
-                          {job?.attributes.salary ? (
-                            <button className='border dark:border-neutral-400/20 rounded-full px-[10px] py-[3px] max-w-[200px] font-semibold text-[13px] flex items-center justify-center mr-[8px]'>
-                              <BanknotesIcon className='w-6 h-6  mr-[0.5rem]' /> {job?.attributes.salary}
-                            </button>
-                          ) : null}
->>>>>>> 36ade5e (general ui updates)
+                          <button className='border border-neutral/40  rounded-full  py-[8px] px-[16px]  h-[4vh] font-medium text-[16px] flex items-center justify-start'>
+                            Apply
+                          </button>
+                          <button className='border border-neutral/40  rounded-full  py-[8px] px-[16px]  h-[4vh] font-medium text-[16px] flex items-center justify-start'>
+                            Report
+                          </button>
                         </div>
                       </div>
                     </div>
@@ -165,83 +126,15 @@ export default function List(data: any) {
                     <p className='font-regular text-[15px]'>{job?.attributes.description}</p>
                     <br />
                     <label className='text-body font-medium leading-[2rem]'>Qualifications</label>
-                    <div className='list-disc font-regular text-[15px]' dangerouslySetInnerHTML={getMarkdownText(job?.attributes?.qualifications)}></div>
+                    <div
+                      className='list-disc font-regular text-[15px]'
+                      dangerouslySetInnerHTML={getMarkdownText(job?.attributes?.qualifications)}
+                    ></div>
                     <br />
                     <label className='text-body font-medium leading-[2rem] '>Job Duties</label>
                     <div className='list-disc font-regular text-[15px]' dangerouslySetInnerHTML={getMarkdownText(job?.attributes?.job_duties)}></div>
                   </div>
                 ))}
-<<<<<<< HEAD
-=======
-              </ul>
-
-              {/* <div className='col-span-2'>
-                <div className='w-full bg-base-100 shadow-md shadow-gray-200/50  border border-neutral-300 dark:border-neutral-400/10 rounded-xl h-[40vh] flex items-center justify-center'>
-                  <div className='w-[90%] flex flex-col items-start justify-start'>
-                    <div className='border dark:border-neutral-400/10 p-[1rem] rounded-full flex items-center justify-center mb-[2rem]'>
-                      <EnvelopeIcon className='w-7 h-7' />
-                    </div>
-                    <h5 className='font-semibold text-[16px] leading-[4rem] text-indigo-700 dark:text-neutral-400'>Subscribe for daily jobs</h5>
-                    <p className='text-[14px] font-regular'>Subscribe to our newsletter to get our latest job postings directly in your inbox.</p>
-                    <form className='w-full flex flex-col mt-[2rem]'>
-                      <input className='w-full border rounded-lg p-[1rem] bg-transparent dark:border-base-100/10'></input>
-                      <button className='bg-indigo-700 text-base-100 dark:bg-base-content/30 dark:text-neutral-300 p-[1rem] mt-[1rem] text-[14px] font-medium rounded-lg'>
-                        Subscribe
-                      </button>
-                    </form>
-                  </div>
-                </div>
-
-                <div className='mt-[2rem]'>
-                  <div className='flex flex-col items-start justify-center'>
-                    <h5 className='font-semibold text-neutral-800 dark:text-neutral-400 text-[15px] leading-[5rem]'>Popular careers</h5>
-                    <div className='flex flex-wrap gap-[0.5rem]'>
-                      {buttons.map((button, i) => {
-                        return (
-                          <button
-                            key={i}
-                            className='bg-gray-100 text-neutral-800 dark:bg-base-content/30 dark:text-neutral-400 p-[1rem] text-base rounded-full font-semibold '
-                          >
-                            {button.label}
-                          </button>
-                        );
-                      })}
-                    </div>
-                  </div>
-                </div>
-              </div> */}
-
-              {/* <div className='grid col-span-1 border bg-white border-base-content border-[3px] p-[3rem] overflow-auto'>
-                <div>
-                  <div className='mb-[4rem]'>
-                    <h1 className='text-[30px] font-regular text-base-content leading-[5rem]'>{currentJob?.attributes.job_title}</h1>
-                    <h6 className='text-[14px] font-regular text-base-content leading-[2rem]'>
-                      {currentJob?.attributes.company_id?.data?.attributes?.company_name}
-                    </h6>
-                    <h6 className='text-[14px] font-regular  leading-[2rem]'>{currentJob?.attributes.location}</h6>
-                    <button className='bg-primary  text-neutral font-semibold hover:bg-primary-focus text-[16px] h-[4rem] w-[8rem] rounded-full mt-[3rem]'>
-                      Apply
-                    </button>
-                  </div>
-                  <label className='text-[16px] font-medium leading-[3rem]'>Job Description</label>
-                  <p className='text-[14px]'>{currentJob?.attributes.description}</p>
-                  <br />
-                  <br />
-                  <br />
-                  <label className='text-[16px] font-medium leading-[3rem]'>Qualifications</label>
-                  <div
-                    className='text-[14px] list-disc'
-                    dangerouslySetInnerHTML={getMarkdownText(data?.data?.data?.find((job: any, i: number) => i === index).attributes?.qualifications)}
-                  ></div>
-                  <br /> <br />
-                  <label className='text-[16px] font-medium leading-[3rem] '>Job Duties</label>
-                  <div
-                    className='text-[14px]'
-                    dangerouslySetInnerHTML={getMarkdownText(data?.data?.data?.find((job: any, i: number) => i === index).attributes?.job_duties)}
-                  ></div>
-                </div>
-              </div> */}
->>>>>>> 36ade5e (general ui updates)
             </div>
           </div>
         </div>
